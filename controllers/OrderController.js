70838498a6fa7f29d00b05e7f07e50382d96a371
Mimi <mariabikarska2988@gmail.com>
2024-08-05 20:50:24 +0300

    Fix order status change

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -1,6 +1,8 @@
 const {
   processOrder,
   getDetailedOrdersForCustomer,
+  getOrderById,
+  changeOrderStatus,
 } = require("../services/OrderService");
 
 module.exports = {
@@ -18,9 +20,39 @@ module.exports = {
       }
     },
     getOrdersByCustomer: async (req, res) => {
+      console.log("here getORDERS BY CUSTOMER");
       const id = req.user.id;
+      console.log(id);
       const orders = await getDetailedOrdersForCustomer(id);
       return res.status(200).json({ orders: orders });
+    },
+    getCustomerOrderById: async (req, res) => {
+      console.log("here GET CUSTOMER DETAILED ORDER");
+
+      const customerId = req.user.id;
+      const orderId = req.params.orderId;
+      try {
+        const order = await getOrderById(customerId, orderId);
+        return res.status(200).json({ order });
+      } catch (err) {
+        return res.status(500).json({ Message: err.message });
+      }
+    },
+    changeOrderStatus: async (req, res) => {
+      const id = req.user.id;
+      if (!id) {
+        return res.status(401).json({ Message: "Not authorized" });
+      }
+      try {
+        const updatedOrder = await changeOrderStatus(
+          req.params.orderId,
+          id,
+          req.body.status
+        );
+        return res.status(200).json({ updatedOrder });
+      } catch (err) {
+        return res.status(500).json({ Message: err.message });
+      }
     },
   },
 };
--- services/OrderService.js
+++ services/OrderService.js
@@ -1,4 +1,5 @@
 const { Customer } = require("../models/Customer");
+const { populate } = require("../models/MedicationClass");
 const { OrderItem, OrderStatusEnum, Order } = require("../models/Order");
 const { PharmaceuticalProduct } = require("../models/PharmaceuticalProduct");
 const { getRandomNumber } = require("../utils/utils");
@@ -55,12 +56,13 @@ const processOrder = async (req, customerId) => {
 
 const getDetailedOrdersForCustomer = async (customerId) => {
   const customer = await Customer.findById(customerId);
+  console.log(customerId);
   if (!customer) {
     throw new Error("Such customer does not exist");
   }
-  let orders = await Order.$where(
-    (customer) => customer.toString() === customerId
-  )
+  const custId = new mongoose.Types.ObjectId(customerId);
+  console.log(custId);
+  let orders = await Order.find({ customer: custId })
     .populate({
       path: "items",
       populate: {
@@ -89,7 +91,103 @@ const getDetailedOrdersForCustomer = async (customerId) => {
   }));
   return orders;
 };
+
+const getOrderById = async (customerId, orderId) => {
+  console.log(customerId);
+  console.log(orderId);
+  const customer = await Customer.findById(customerId);
+  if (!customer) {
+    throw new Error("Customer doesn't exist");
+  }
+  const order = await Order.findOne({ number: orderId })
+    .populate({ path: "customer", select: ["phoneNumber", "detailedAddress"] })
+    .populate({
+      path: "items",
+      populate: {
+        path: "product",
+        populate: {
+          path: "vendor",
+          select: "companyName",
+        },
+      },
+    })
+    .lean();
+  if (!order) {
+    throw Error("Order not found!");
+  }
+  if (order.customer._id.toString() !== customerId) {
+    throw Error("Not authorized to view order details!");
+  }
+  return {
+    ...order,
+    items: order.items.map((item) => {
+      return { ...item.product, quantity: item.quantity };
+    }),
+  };
+};
+
+const changeOrderStatus = async (orderId, customerId, newOrderStatus) => {
+  const customer = await Customer.findById(customerId);
+  if (!customer) {
+    throw Error("Customer doesn't exist");
+  }
+  const order = await Order.findById(orderId);
+  if (!order) {
+    throw Error("Order does not exist");
+  }
+  if (order.status === OrderStatusEnum.Canceled) {
+    throw Error("Order is cancelled already and its status can't be changed!");
+  }
+  if (
+    order.status === OrderStatusEnum.Created &&
+    ![OrderStatusEnum.Canceled, OrderStatusEnum.Created].includes(
+      newOrderStatus
+    )
+  ) {
+    throw Error(
+      "Order status must be changed from created to confirmed first!"
+    );
+  }
+  if (
+    order.status === OrderStatusEnum.Confirmed &&
+    ![OrderStatusEnum.Canceled, OrderStatusEnum.Delivered].includes(
+      newOrderStatus
+    )
+  ) {
+    throw Error(
+      "Order status can be changed from confirmed to delivered or canceled ONLY!"
+    );
+  }
+  if (
+    order.status === OrderStatusEnum.Delivered &&
+    newOrderStatus !== OrderStatusEnum.Completed
+  ) {
+    throw Error(
+      "Order status can be changed from delivered to completed ONLY!"
+    );
+  }
+  if (newOrderStatus === OrderStatusEnum.Confirmed) {
+    order.status = newOrderStatus;
+    order.confirmedOn = Date.now();
+  }
+  if (newOrderStatus === OrderStatusEnum.Canceled) {
+    order.status = newOrderStatus;
+    order.canceledOn = Date.now();
+  }
+  if (newOrderStatus === OrderStatusEnum.Delivered) {
+    order.status = newOrderStatus;
+    order.deliveredOn = Date.now();
+  }
+  if (newOrderStatus === OrderStatusEnum.Completed) {
+    order.status = newOrderStatus;
+    order.canceledOn = Date.now();
+  }
+  const updatedOrder = await order.save();
+  return updatedOrder;
+};
 module.exports = {
   processOrder,
   getDetailedOrdersForCustomer,
+  getOrderById,
+  changeOrderStatus,
 };
